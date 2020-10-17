8af1429c76aa84cae00738facce445e2b551827c
Jörg <joerg.sesterhenn@gmail.com>
2020-10-17 02:01:55 +0200

    #1 refactored to use classes

---
+++ TicTacToeModel.py
@@ -0,0 +1,69 @@
+class TicTacToeModel:
+    """The tic-tac-toe model"""
+    initial_data = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
+    player_one_sign = "o"
+    player_two_sign = "x"
+
+    def __init__(self):
+        self.data = self.initial_data
+        self.active_player = "eins"
+
+    def set_mark(self, x_position, y_position):
+        self.data[x_position][y_position] = self.get_active_player_sign()
+
+    def get_data(self):
+        return self.data
+
+    def get_active_player_sign(self):
+        if self.active_player == "eins":
+            return self.player_one_sign
+        else:
+            return self.player_two_sign
+
+    def switch_player(self):
+        if self.active_player == "eins":
+            self.active_player = "zwei"
+        else:
+            self.active_player = "eins"
+
+    def check_won(self):
+        return (self.data[0][0] + self.data[1][0] + self.data[2][0] ==
+                self.player_one_sign + self.player_one_sign + self.player_one_sign or
+                self.data[0][1] + self.data[1][1] + self.data[2][1] ==
+                self.player_one_sign + self.player_one_sign + self.player_one_sign or
+                self.data[0][2] + self.data[1][2] + self.data[2][2] ==
+                self.player_one_sign + self.player_one_sign + self.player_one_sign or
+
+                self.data[0][0] + self.data[0][1] + self.data[0][2] ==
+                self.player_one_sign + self.player_one_sign + self.player_one_sign or
+                self.data[1][0] + self.data[1][1] + self.data[1][2] ==
+                self.player_one_sign + self.player_one_sign + self.player_one_sign or
+                self.data[2][0] + self.data[2][1] + self.data[2][2] ==
+                self.player_one_sign + self.player_one_sign + self.player_one_sign or
+
+                self.data[0][0] + self.data[1][1] + self.data[2][2] ==
+                self.player_one_sign + self.player_one_sign + self.player_one_sign or
+                self.data[0][2] + self.data[1][1] + self.data[2][0] ==
+                self.player_one_sign + self.player_one_sign + self.player_one_sign or
+
+                self.data[0][0] + self.data[1][0] + self.data[2][0] ==
+                self.player_two_sign + self.player_two_sign + self.player_two_sign or
+                self.data[0][1] + self.data[1][1] + self.data[2][1] ==
+                self.player_two_sign + self.player_two_sign + self.player_two_sign or
+                self.data[0][2] + self.data[1][2] + self.data[2][2] ==
+                self.player_two_sign + self.player_two_sign + self.player_two_sign or
+
+                self.data[0][0] + self.data[0][1] + self.data[0][2] ==
+                self.player_two_sign + self.player_two_sign + self.player_two_sign or
+                self.data[1][0] + self.data[1][1] + self.data[1][2] ==
+                self.player_two_sign + self.player_two_sign + self.player_two_sign or
+                self.data[2][0] + self.data[2][1] + self.data[2][2] ==
+                self.player_two_sign + self.player_two_sign + self.player_two_sign or
+
+                self.data[0][0] + self.data[1][1] + self.data[2][2] ==
+                self.player_two_sign + self.player_two_sign + self.player_two_sign or
+                self.data[0][2] + self.data[1][1] + self.data[2][0] ==
+                self.player_two_sign + self.player_two_sign + self.player_two_sign)
+
+    def coordinates_taken(self, a, b):
+        return self.data[a][b] != " "
