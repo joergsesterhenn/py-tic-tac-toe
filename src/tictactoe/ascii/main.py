c4ad0edb3b3a9dfb4a05f0514e857d4cdda8e053
Jörg Sesterhenn <joerg.sesterhenn@gmail.com>
2021-05-27 00:07:15 +0200

    add docs to main.py

--- src/tictactoe/ascii/main.py
+++ src/tictactoe/ascii/main.py
@@ -2,5 +2,6 @@ from tictactoe.ascii.tic_tac_toe_game_controller import TicTacToeGameController
 
 
 def run():
+    """Run the ascii App."""
     controller = TicTacToeGameController()
     controller.run()
--- src/tictactoe/flask/main.py
+++ src/tictactoe/flask/main.py
@@ -4,6 +4,7 @@ from tictactoe.flask.tic_tac_toe_game_controller import TicTacToeGameController
 
 
 def run():
+    """Run the flask App."""
     image_path = os.path.abspath("images")
     controller = TicTacToeGameController("TicTacToe", image_path)
     controller.run()
